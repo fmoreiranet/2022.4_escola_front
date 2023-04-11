201ee023bbb0669a6eb66a57c834df6b7fdca798
fMoreira <fabianomoreira.net@gmail.com>
2023-04-11 19:52:37 -0300

    updates crud front

--- js/model/AlunoClass.js
+++ js/model/AlunoClass.js
@@ -1,40 +1,23 @@
 class Aluno {
     constructor() {
-        this.id_aluno = 0,
+        this.matricula = 0,
             this.nome = "",
-            this.matricula = "",
-            this.turma = "",
-            this.avaliacao1 = 0,
-            this.avaliacao2 = 0,
-            this.media = 0,
-            this.resultado = ""
+            this.email = "",
+            this.senha = "",
+            this.data_nasc = "",
+            this.ativo = true
     }
     validarDados() {
         let erros = "";
         if (this.nome) {
             erros += "Nome em branco.\n";
         }
-        if (this.avaliacao1 < 0 || this.avaliacao1 > 10 || Number.isNaN(this.avaliacao1)) {
-            erros += "Valor da Avaliação 1 esta fora do padrão (0,0 a 10,0).\n";
+        if (this.email) {
+            erros += "E-mail em branco.\n";
         }
-        if (this.avaliacao2 < 0 || this.avaliacao2 > 10 || Number.isNaN(this.avaliacao2)) {
-            erros += "Valor da Avaliação 2 esta fora do padrão (0,0 a 10,0).\n";
+        if (this.senha) {
+            erros += "Senha em branco.\n";
         }
-        return erros;
-    };
-    calculoMedia() {
-        this.media = (this.avaliacao1 + this.avaliacao2) / 2;
-        return this.media;
-    };
-    validarResultado() {
-        let result = ""
-        if (this.media >= 6) {
-            result = "Aprovado";
-        } else if (this.media >= 3 && this.media < 6) {
-            result = "Exame";
-        } else {
-            result = "Reprovado";
-        }
-        this.resultado = result;
+        if (erros != "") throw new Exception(erros);
     };
 }
--- js/services/alunoService.js
+++ js/services/alunoService.js
@@ -1,88 +1,65 @@
 //CRUD ----------------------
-function addAluno(aluno = {}) {
-    aluno.id_aluno = new Date().getTime();
-    alunos.push(aluno);
-    localStorage.setItem("alunos", JSON.stringify(alunos));
+function addAluno(aluno) {
+    console.log("Cadastrado");
 }
 
-function getAllAluno() { //listAluno()
-    let dadosAlunos = JSON.parse(localStorage.getItem("alunos"));
-    if (dadosAlunos == null) {
-        dadosAlunos = [];
-    }
-    return dadosAlunos;
+function getAllAluno() {
+
 }
 
-function getAluno(index = 0) {
-    let dadosAlunos = getAllAluno();
-    let aluno = dadosAlunos[index];
-    return aluno;
+function getAluno(matricula) {
+
 }
 
-function deleteAluno(index = 0) {
-    let dadosAlunos = getAllAluno();
-    let novoVetor = [];
+function deleteAluno() {
     let pergunta = `Deseja remover o aluno ${dadosAlunos[index].nome}?`;
     if (confirm(pergunta)) {
-        //dadosAlunos.splice(index, 1);
-        let cont = 0;
-        for (let atual = 0; atual < (dadosAlunos.length - 1); atual++) {
-            if (atual != index) {
-                novoVetor[cont] = dadosAlunos[atual];
-                cont++;
-            }
-        }
-        // localStorage.clear();
-        localStorage.setItem("alunos", JSON.stringify(novoVetor));
-        montarTabela();
+
     }
     return novoVetor;
 }
 
 function updateAluno(aluno) {
-    let dadosAlunos = getAllAluno();
-    for (let index = 0; index < dadosAlunos.length; index++) {
-        if (dadosAlunos[index].id_aluno == aluno.id_aluno) {
-            dadosAlunos[index] = aluno;
-        }
-    }
-    localStorage.setItem("alunos", JSON.stringify(dadosAlunos));
+
 }
 
-//Busca CEP ------------------
-function buscaCEP(cep = "", callback) {
-    let url = `https://viacep.com.br/ws/${cep}/json/`;
-    const http = new XMLHttpRequest();
-    http.onload = function (res) {
-        let endereco = JSON.parse(res.target.response);
-        return callback(endereco);
-    };
-    http.open("GET", url);
-    http.send();
-}
 
-//https://www.w3schools.com/js/js_async.asp
-//https://www.w3schools.com/js/js_api_fetch.asp
-async function buscaCEP2(cep = "", callback) {
-    let url = `https://viacep.com.br/ws/${cep}/json/`;
 
-    //https://developer.mozilla.org/pt-BR/docs/Web/API/Fetch_API/Using_Fetch
-    //Definimos o objeto com os parâmetros de envio da requisição 
-    var parameter = {
-        method: 'GET'
-    };
 
-    //buscar dados de um arquivo ou url
-    await fetch(url, parameter)
-        //processa os dados de textos recebidos. O termino depende do tamanho dos dados
-        .then(res => res.text())
-        //pegar os dados do processamento do 'then' anterior para se convertido para um Objeto;
-        .then(txt => {
-            let endereco = JSON.parse(txt);
-            return callback(endereco);
-        })
-        //Erro na tratativa dos dados. Os then anteriores;
-        .catch(err => {
-            console.error("Erro:" + err.message);
-        })
-}
+// //Busca CEP ------------------
+// function buscaCEP(cep = "", callback) {
+//     let url = `https://viacep.com.br/ws/${cep}/json/`;
+//     const http = new XMLHttpRequest();
+//     http.onload = function (res) {
+//         let endereco = JSON.parse(res.target.response);
+//         return callback(endereco);
+//     };
+//     http.open("GET", url);
+//     http.send();
+// }
+
+// //https://www.w3schools.com/js/js_async.asp
+// //https://www.w3schools.com/js/js_api_fetch.asp
+// async function buscaCEP2(cep = "", callback) {
+//     let url = `https://viacep.com.br/ws/${cep}/json/`;
+
+//     //https://developer.mozilla.org/pt-BR/docs/Web/API/Fetch_API/Using_Fetch
+//     //Definimos o objeto com os parâmetros de envio da requisição 
+//     var parameter = {
+//         method: 'GET'
+//     };
+
+//     //buscar dados de um arquivo ou url
+//     await fetch(url, parameter)
+//         //processa os dados de textos recebidos. O termino depende do tamanho dos dados
+//         .then(res => res.text())
+//         //pegar os dados do processamento do 'then' anterior para se convertido para um Objeto;
+//         .then(txt => {
+//             let endereco = JSON.parse(txt);
+//             return callback(endereco);
+//         })
+//         //Erro na tratativa dos dados. Os then anteriores;
+//         .catch(err => {
+//             console.error("Erro:" + err.message);
+//         })
+// }
