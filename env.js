767eac97faac21ba7be87ef6a9673164bfc665be
fMoreira <fabianomoreira.net@gmail.com>
2023-05-18 19:49:35 -0300

    conecta x10host

---
+++ env.js
@@ -0,0 +1,8 @@
+const process = {
+    env: {
+        URL_API: "https://api.fabianomoreira.x10.mx/",
+    }
+}
+//http://localhost/projetos/escola_back/
+//https://escolaweb2.000webhostapp.com/api/
+//https://api.fabianomoreira.x10.mx/
--- js/env.js
+++
@@ -1,7 +0,0 @@
-const process = {
-    env: {
-        URL_API: "http://localhost/projetos/escola_back/",
-    }
-}
-//http://localhost/projetos/escola_back/
-//https://escolaweb2.000webhostapp.com/api/
