a04a1e7baac0ea08e275677e7e37f77abfe47002
fMoreira <fabianomoreira.net@gmail.com>
2023-05-18 23:59:03 -0300

    update

---
+++ js/main.js
@@ -0,0 +1,15 @@
+import AlunoController from "./controller/AlunoController.js"
+
+const alunoController = new AlunoController;
+
+
+// Loadding.......
+function loadingShow() {
+    let loading = document.querySelector("#loading");
+    loading.classList.remove("visually-hidden");
+}
+
+function loadingHide() {
+    let loading = document.querySelector("#loading");
+    loading.classList.add("visually-hidden");
+}
