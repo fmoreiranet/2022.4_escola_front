fb240f074be7973bcafd5a3da3d6896f2980e6d6
fMoreira <fabianomoreira.net@gmail.com>
2023-05-19 01:15:35 -0300

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-import AlunoController from "/assets/js/controller/AlunoController.js"
+import AlunoController from "../assets/js/controller/AlunoController.js"
 
 const alunoController = new AlunoController;
 
